2ad60745b2ab63cc3c21b8878c9c37bc9eb69078
maru_tirth <tirthwohlig@gmail.com>
2025-06-29 19:42:04 +0530

    Add initial Java files for basic programming concepts

---
+++ Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("a + b = " + (a + b)); // Addition
+        System.out.println("a - b = " + (a - b)); // Subtraction
+        System.out.println("a * b = " + (a * b)); // Multiplication
+        System.out.println("b / a = " + (b / a)); // Division
+        System.out.println("b % a = " + (b % a)); // Modulus
+    }
+}
---
+++ Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "Tirth Maru";
+        int age = 19;
+        double height = 190;
+        char Grade = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " cm");
+        System.out.println("Grade: " + Grade);
+        System.out.println("Is Java fun? " + isJavaFun);
+    }
+}
